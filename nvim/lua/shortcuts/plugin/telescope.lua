860b817ed6c0f102d810e6a4fe3ef7e2c2c7b6eb
shortcuts <vannicattec@gmail.com>
2022-11-25 22:03:48 +0100

    feat: vertical telescope

--- nvim/lua/shortcuts/plugin/telescope.lua
+++ nvim/lua/shortcuts/plugin/telescope.lua
@@ -18,22 +18,19 @@ require("telescope").setup({
 				["<S-CR>"] = ACTIONS.select_vertical,
 			},
 		},
+		layout_strategy = "vertical",
 		layout_config = {
-			horizontal = {
-				preview_width = 0.5,
-				results_width = 0.85,
-			},
 			vertical = {
-				mirror = false,
+				height = 0.9,
+				preview_cutoff = 0,
+				preview_height = 0.6,
+				results_height = 0.3,
+				width = 0.7,
 			},
-			width = 0.90,
-			height = 0.90,
-			preview_cutoff = 120,
-			preview_width = 150,
 		},
 
 		-- common files to ignore
-		file_ignore_patterns = { "^.git/", "^node_modules/", "^plugged/" },
+		file_ignore_patterns = { "^.git/", "^node_modules/" },
 
 		-- default arguments with `--hidden` added to search in hidden files
 		vimgrep_arguments = {
@@ -65,16 +62,17 @@ require("telescope").load_extension("file_browser")
 local T = {}
 
 local delta = PREVIEWERS.new_termopen_previewer({
+	title = "Git diff",
 	get_command = function(entry)
-		return { "git", "-c", "core.pager=delta", "-c", "delta.side-by-side=false", "diff", entry.path }
+		return { "git", "-c", "core.pager=delta", "diff", entry.path }
 	end,
 })
 
 T.my_git_status = function(opts)
-    opts = {
-        previewer = delta,
-        initial_mode = "normal",
-    }
+	opts = {
+		previewer = delta,
+		initial_mode = "normal",
+	}
 
 	BUILTIN.git_status(opts)
 end
