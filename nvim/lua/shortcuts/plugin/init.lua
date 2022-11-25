cef137907ad1c6d3e103be37a3ad3751c6aac401
shortcuts <vannicattec@gmail.com>
2022-11-25 23:43:50 +0100

    fix: nnp local import

--- nvim/lua/shortcuts/plugin/init.lua
+++ nvim/lua/shortcuts/plugin/init.lua
@@ -9,5 +9,6 @@ require("shortcuts.plugin.treesitter")
 require("shortcuts.plugin.telescope")
 
 -- personal
-require("no-neck-pain").setup()
 -- vim.opt.rtp:append(os.getenv("HOME") .. "/Documents/no-neck-pain.nvim")
+-- require("no-neck-pain").setup({ debug = true })
+require("no-neck-pain").setup()
