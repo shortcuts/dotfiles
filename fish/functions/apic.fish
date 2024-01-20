17f53db21c1efdd27cb7e6e828e7d3c7801b0b44
shortcuts <vannicattec@gmail.com>
2024-01-20 18:59:38 +0100

    fix: apic methods and rpi submodules

--- fish/functions/apic.fish
+++ fish/functions/apic.fish
@@ -1,3 +1,3 @@
 function apic
-  cd /Users/clement.vannicatte/Documents/api-clients-automation/scripts && NODE_NO_WARNINGS=1 node dist/scripts/cli/index.js $argv
+  cd /Users/clement.vannicatte/Documents/api-clients-automation/scripts && nvm use && NODE_NO_WARNINGS=1 node dist/scripts/cli/index.js $argv && cd ..
 end
