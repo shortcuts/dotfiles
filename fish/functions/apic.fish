3546584e95036d7b5c3bbf7f677c3e7a8ce83e23
shortcuts <vannicattec@gmail.com>
2024-08-27 10:57:01 +0200

    chore: upgrade repo

--- fish/functions/apic.fish
+++
@@ -1,3 +0,0 @@
-function apic
-  cd /Users/clement.vannicatte/Documents/api-clients-automation/scripts && nvm use && NODE_NO_WARNINGS=1 node dist/scripts/cli/index.js $argv && cd ..
-end
--- fish/functions/apicb.fish
+++ fish/functions/apicb.fish
@@ -1,3 +1,3 @@
 function apicb
-  cd /Users/clement.vannicatte/Documents/api-clients-automation/scripts && nvm use && yarn build:cli && NODE_NO_WARNINGS=1 node dist/scripts/cli/index.js $argv && cd ..
+  cd /Users/clement.vannicatte/Documents/api-clients-automation/scripts && nvm use && yarn build:cli && yarn start $argv && cd ..
 end
