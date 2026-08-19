2903a980de22bde025c9ccaec596c2f482879c21
shortcuts <vannicattec@gmail.com>
2026-08-19 22:15:06 +0200

    feat: git diff view

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -16,7 +16,6 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         config = true,
         keys = {
-            { "<Leader>glb", "<cmd>Gitsigns toggle_current_line_blame<CR>" },
             { "<Leader>gb", "<cmd>Gitsigns blame<CR>" },
         },
     },
@@ -30,4 +29,20 @@ return {
             { "<Leader>go", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
         },
     },
+    {
+        "dlyongemallo/diffview-plus.nvim",
+        version = "*",
+        keys = {
+            { "<Leader>gdo", "<cmd>DiffviewOpen<cr>", mode = { "n", "v" }, desc = "open git diff" },
+            { "<Leader>gdq", "<cmd>DiffviewClose<cr>", mode = { "n", "v" }, desc = "close git diff" },
+            { "<Leader>gsh", "<cmd>DiffviewOpen @<cr>", mode = { "n", "v" }, desc = "close git diff" },
+        },
+        cmd = {
+            "DiffviewOpen",
+            "DiffviewToggle",
+            "DiffviewFileHistory",
+            "DiffviewDiffFiles",
+            "DiffviewLog",
+        },
+    }
 }
