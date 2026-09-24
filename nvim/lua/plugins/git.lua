99a8a97d9832a927f7df3a3a2f489ee5ae258942
shortcuts <vannicattec@gmail.com>
2026-09-24 22:50:15 +0200

    feat: git diff in nvim

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -31,17 +31,57 @@ return {
     {
         "dlyongemallo/diffview-plus.nvim",
         version = "*",
+        init = function()
+            vim.api.nvim_create_user_command("Git", function(o)
+                local sub, rest = o.fargs[1], table.concat(vim.list_slice(o.fargs, 2), " ")
+                if sub == "diff" then
+                    vim.cmd("DiffviewOpen " .. rest)
+                elseif sub == "show" then
+                    -- `rev^!` limits diffview to the changes of that one commit, like `git show`.
+                    vim.cmd("DiffviewOpen " .. (rest == "" and "HEAD" or rest) .. "^!")
+                elseif sub == "log" then
+                    local range = o.range > 0 and (o.line1 .. "," .. o.line2) or ""
+                    vim.cmd(range .. "DiffviewFileHistory " .. rest)
+                elseif sub == "stash" and rest == "" then
+                    vim.cmd("DiffviewFileHistory -g --range=stash")
+                else
+                    vim.cmd("!git " .. o.args)
+                end
+            end, { nargs = "+", range = true })
+            -- User commands must start uppercase, so expand a typed `:git` to `:Git`.
+            for lhs, rhs in pairs({ git = "Git", gd = "Git diff", gsh = "Git show" }) do
+                vim.cmd(
+                    ("cnoreabbrev <expr> %s getcmdtype() == ':' && getcmdline() ==# '%s' ? '%s' : '%s'"):format(
+                        lhs,
+                        lhs,
+                        rhs,
+                        lhs
+                    )
+                )
+            end
+        end,
+        opts = function()
+            local cycle = {
+                {
+                    "n",
+                    "<Leader>gx",
+                    require("diffview.actions").cycle_layout,
+                    { desc = "Cycle layouts" },
+                },
+            }
+            return {
+                view = {
+                    cycle_layouts = {
+                        default = { "diff2_horizontal", "diff1_plain", "diff1_inline" },
+                    },
+                },
+                keymaps = { view = cycle, file_panel = cycle, file_history_panel = cycle },
+            }
+        end,
         keys = {
-            { "<Leader>gdo", "<cmd>DiffviewOpen<cr>", mode = { "n", "v" }, desc = "open git diff" },
             {
-                "<Leader>gdq",
+                "<Leader>gq",
                 "<cmd>DiffviewClose<cr>",
-                mode = { "n", "v" },
-                desc = "close git diff",
-            },
-            {
-                "<Leader>gsh",
-                "<cmd>DiffviewOpen HEAD~1<cr>",
                 mode = { "n", "v" },
                 desc = "close git diff",
             },
